e2d627470d3e1cd79498f872d656210b25fbe62e
Azunyan <s15a1302@bene.fit.ac.jp>
2017-06-27 10:16:35 +0900

    add server file. add tenPercent Page. #2

--- server.go
+++ server.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
+var test_int = 10000.0
+
+func tenPercent(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "110%% = %f\n", test_int * 1.1)
+	fmt.Fprintf(w, " 90%% = %f\n", test_int * 0.9)
+}
+
 func main() {
+	fmt.Print(" \n ---Hello World---\n")
+	http.HandleFunc("/", tenPercent)
+	http.ListenAndServe(":8080", nil)
 }
